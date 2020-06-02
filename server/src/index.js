e20e08de08d996a16ece1d2da0f7b48c0f503e0e
sheygs13 <segun.ekoh@gmail.com>
2020-06-02 20:51:19 +0100

    implement signin route

--- server/src/index.js
+++ server/src/index.js
@@ -11,3 +11,4 @@ app.use('/api/v1', users);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -33,7 +33,34 @@ router.post('/auth/signup', async (req, res) => {
 
    } catch ({ message }) {
       console.error(message);
-      res.status(500).send('server error');
+      res.status(500).send('Server error');
+   }
+});
+
+
+router.post('/auth/signin', async (req, res) => {
+   try {
+      // 1. destructure req.body
+      const { email, password } = req.body;
+
+      // 2. validate email & password
+      if (!email || !password ) return res.status(400).json({ message: 'All fields required' })
+
+      // 3. throw error if user doesn't exists
+      const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+      if (user.rows.length === 0) return res.status(400).json({ message: 'Invalid Email' })
+
+      // 4. check if incoming password is same as db password
+      const valid = await bcrypt.compare(password, user.rows[0].password);
+      if (!valid) return res.status(400).json({ message: 'Invalid Password' });
+
+      // 5. send jwt token back to user.
+      const token = generateJwt(user.rows[0]._id);
+      res.status(200).json({ token });
+
+   } catch ({ message }) {
+      console.error(message);
+      res.status(500).send('Server error');
    }
 });
 
