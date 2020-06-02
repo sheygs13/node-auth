2f2bd648cdd2210058e7d117576dd84df0837845
sheygs13 <segun.ekoh@gmail.com>
2020-06-02 23:47:39 +0100

    add dashboard router to index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,21 @@
 import cors from 'cors';
 import express from 'express';
 import users from './routes/users';
+import dashboard from './routes/dashboard';
 const app = express();
 
 app.use(cors());
+
 // req.body
 app.use(express.json());
+
+// signin and signup routes
 app.use('/api/v1', users);
 
-const port = process.env.PORT || 3000;
+// dashboard
+app.use('/api/v1', dashboard)
+
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
