11a93121053ea9b3543e8670ac79257aaff739bf
sheygs13 <segun.ekoh@gmail.com>
2020-06-02 18:00:25 +0100

    :rocket: application entry...

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import cors from 'cors';
+import express from 'express';
+import users from './routes/users';
+const app = express();
+
+app.use(cors());
+// req.body
+app.use(express.json());
+app.use('/api/v1', users);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
