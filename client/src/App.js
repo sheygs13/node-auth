619384172f6fc3c7e06516158ca8dea16ab35fe1
sheygs13 <segun.ekoh@gmail.com>
2020-06-08 12:58:34 +0100

    starter files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,65 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
+import Dashboard from './components/Dashboard';
+import Register from './components/Register';
+import Login from './components/Login';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect 
+} from 'react-router-dom';
+
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = bool => setIsAuthenticated(bool);
+
   return (
     <Fragment>
-            <h1>Hello React</h1>
+      <Router>
+          <div className="container">
+            <Switch>
+               <Route 
+                 exact 
+                 path="/login" 
+                 render= {
+                   props => 
+                    !isAuthenticated ? (
+                      <Login {...props} setAuth={setAuth}/>
+                    ) : ( 
+                      <Redirect to= "/dashboard"/>
+                    ) 
+                  } 
+                 />
+               <Route 
+                 exact 
+                 path="/register" 
+                 render= {
+                   props => 
+                    !isAuthenticated ? ( 
+                      <Register {...props} setAuth={setAuth}/>
+                    ) : ( 
+                      <Redirect to="/login"/>
+                    ) 
+                  } 
+                 />
+               <Route 
+                 exact 
+                 path="/dashboard" 
+                 render= {
+                   props => 
+                    isAuthenticated ? ( 
+                      <Dashboard {...props} setAuth = {setAuth}/>
+                    ) : (
+                      <Redirect to="/login" />
+                    )
+                  } 
+                />
+            </Switch>
+          </div>
+      </Router>
     </Fragment>
   );
 }
